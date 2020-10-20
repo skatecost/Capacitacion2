fbce0747291735520c79d27dbf6c2b077399b9b0
skatecost <skatecost1@gmail.com>
2020-10-20 07:55:12 -0500

    comit v1.0

---
+++ src/main/java/com/capacitacion2/capacitacion2/App.java
@@ -0,0 +1,21 @@
+package com.capacitacion2.capacitacion2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        int key = 0 ;
+        switch(key) {
+        case 1:
+        	break;
+        
+        }
+        
+        
+    }
+}
