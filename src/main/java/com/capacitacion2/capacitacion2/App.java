3c4aceb1cf52dd971610b0ca4ecdaacae1b9e07b
skatecost <skatecost1@gmail.com>
2020-10-23 01:43:03 -0500

    V1.1 caja de texto gmail

--- src/main/java/com/capacitacion2/capacitacion2/App.java
+++ src/main/java/com/capacitacion2/capacitacion2/App.java
@@ -1,22 +1,17 @@
 package com.capacitacion2.capacitacion2;
 
+import com.capacitacion2.case3.AutomatizacionPaginaWeb;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.print("Realice un cambio");
-        int key = 0 ;
-        switch(key) {
-        case 1:
-        	break;
-        
-        }
-        
-        
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("instancio");
+		 AutomatizacionPaginaWeb aut = new AutomatizacionPaginaWeb();
+		System.out.println("instancio");
+		 aut.interactuarConGmail();
+
+	}
 }
---
+++ src/main/java/com/capacitacion2/case3/AutomatizacionPaginaWeb.java
@@ -0,0 +1,28 @@
+package com.capacitacion2.case3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class AutomatizacionPaginaWeb {
+
+	
+	public void interactuarConGmail () {
+
+	String ubicacionDriver = "src\\main\\java\\resources\\drivers\\chromedriver.exe";
+	String url ="https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin";
+	System.setProperty("webdriver.chrome.driver", ubicacionDriver);
+	WebDriver driver = new ChromeDriver();
+	driver.get(url);
+	WebElement cajaTeextoNombre = driver.findElement(By.id("identifierId"));
+	cajaTeextoNombre.sendKeys("ing.cristhianher@gmail.com");
+	driver.close();
+	try {
+		Thread.sleep(5000);
+	}catch(Exception e){
+				
+	}
+	}
+}
